5239e965159c32227036774bbd68756fa025ba1c
riskerGoldClass <roakfwk30@naver.com>
2022-12-20 19:55:43 +0900

    update : Home 페이지 해상도 코멘트 추가

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,27 +5,31 @@ import TypeIt from "typeit-react";
 function Home(props) {
     return (
 
-        <div className="home">
-            <div>
-                <p>
-                    <TypeIt
-                        getBeforeInit={(instance) => {
-                            instance.pause(500).type("Hello! I'm risker ")
-                                .pause(1200)
-                                    .type("and..")
-                                .pause(1500)
-                                    .delete(23)
-                                .pause(1000)
-                                    .type("Trouble Shooter <img id='gun' src='icons/icon-gun.png'/> ");
-                            return instance;
-                        }}
-                    />
-                </p>
-                <p>안효원의 포트폴리오</p>
-                <p>Back-End Developer</p>
+        <>
+            <div className="home">
+                <div>
+                    <p>
+                        <TypeIt
+                            getBeforeInit={(instance) => {
+                                instance.pause(500).type("Hello! I'm risker ")
+                                    .pause(1200)
+                                        .type("and..")
+                                    .pause(1500)
+                                        .delete(23)
+                                    .pause(1000)
+                                        .type("Trouble Shooter <img id='gun' src='icons/icon-gun.png'/> ");
+                                return instance;
+                            }}
+                        />
+                    </p>
+                    <p>안효원의 포트폴리오</p>
+                    <p>Back-End Developer</p>
+                </div>
             </div>
-        </div>
-    );
+            <p className="comment">This page is optimized for 1080p or higher</p>
+        </>
+
+);
 }
 
 export default Home;
